57e8bdb3431c3ac634693bcaf8f9443e38dbd565
A571GT PC <liongethanex@gmail.com>
2024-07-06 23:40:06 +0700

    1

--- BankingMain.java
+++ BankingMain.java
@@ -1,13 +1,31 @@
+import java.util.Scanner;
 public class BankingMain{
     public static void main(String[] args){
         System.out.println("Main Menu");
 
-        System.out.println("Add User");
+        System.out.println("Create User");
         Userdata User1 = new Userdata();
         User1.newUserData("Caleb", "password123", 12345, 10, 1000.0);
         System.out.println(User1.getallUserData());
         
         System.out.println("Login");
+            Scanner login = new Scanner(System.in);
+            System.out.println("Username - ");
+            String loginusrinp = login.nextLine();
+            System.out.println("Password - ");
+            String loginpassinp = login.nextLine();
+            //if username and password match
+
+
+            //if appdata file doesn't exist
+                Scanner saveappdata = new Scanner(System.in);
+                System.out.println("Save Login information? - ");
+                String saveappdatausrinp = saveappdata.nextLine();
+                if(saveappdatausrinp.equals("yes") || saveappdatausrinp.equals("y")){
+                    //Create file with appdata
+                    System.out.println("Login information saved");
+                }
+                saveappdata.close();
 
             System.out.println("Update Account Info");
             
@@ -16,6 +34,7 @@ public class BankingMain{
 
 
             System.out.println("Moderator Access");
+
 
 
 
